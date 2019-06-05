d1ec098f54e72975665434988325afd3fccefaec
robertgordon <robertgordon.subscript2@gmail.com>
2019-06-05 16:37:58 -0400

    Added constructor and getExamScores to student

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,50 @@
 package io.zipcoder;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Student {
+
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, Double[] testScores){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        examScores = new ArrayList<>();
+        if(testScores != null) {
+            examScores.addAll(Arrays.asList(testScores));
+        }
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getExamScores(){
+        StringBuilder sbuild = new StringBuilder();
+        DecimalFormat df = new DecimalFormat("#.##");
+        for(int i = 0; i < examScores.size(); i++){
+            sbuild.append(String.format("Exam %d -> %s\n",i+1,df.format(examScores.get(i))));
+        }
+        return sbuild.toString();
+    }
+
+    public Integer getNumberOfExamsTaken(){
+        return examScores.size();
+    }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,81 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StudentTest {
+
+    @Test
+    public void constructorTestFirstName(){
+        Double[] scores = {};
+        Student s = new Student("Maria Theresa","Hapsburg",scores);
+        String expected = "Maria Theresa";
+
+        String actual = s.getFirstName();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void constructorTestLastName(){
+        Double[] scores = {};
+        Student s = new Student("Maria Theresa","Hapsburg",scores);
+        String expected = "Hapsburg";
+
+        String actual = s.getLastName();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void constructorTestNullScores(){
+        Student s = new Student("Maria Theresa", "Hapsburg", null);
+
+        Assert.assertEquals(2,1+1);
+    }
+
+    @Test
+    public void getExamScoresTest1(){
+        Double[] scores = {100d, 90d, 93d};
+        Student s = new Student("Maria Theresa", "Hapsburg", scores);
+        String expected = "Exam 1 -> 100\nExam 2 -> 90\nExam 3 -> 93\n";
+
+        String actual = s.getExamScores();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getExamScoresTest2(){
+        Double[] scores = {100d, 90.4, 89.99, 93.457, 93.454};
+        Student s = new Student("Maria Theresa", "Hapsburg", scores);
+        String expected = "Exam 1 -> 100\nExam 2 -> 90.4\nExam 3 -> 89.99\nExam 4 -> 93.46\nExam 5 -> 93.45\n";
+
+        String actual = s.getExamScores();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getExamScoreTest3(){
+        Double[] scores = {};
+        Student s = new Student("Maria Theresa", "Hapsburg", scores);
+        String expected = "";
+
+        String actual = s.getExamScores();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getExamScoreTest4(){
+        Double[] scores = null;
+        Student s = new Student("Maria Theresa", "Hapsburg", scores);
+        String expected = "";
+
+        String actual = s.getExamScores();
+
+        Assert.assertEquals(expected, actual);
+    }
 
 }
